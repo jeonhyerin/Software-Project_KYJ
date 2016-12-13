e9dd365f2473386620e111cb201a72f8324c435b
jeonhyerin <marie0902@naver.com>
2016-12-13 19:27:49 +0900

    login.sql
    안드로이드에서 로그인을 외부 db랑 연동해서 할 수 있도록 설계한 데이터베이스입니다.

---
+++ login.sql
@@ -0,0 +1,8 @@
+CREATE TABLE login(
+  email char(255) not null,
+  password char(30) not null,
+  id int(11) unsigned not null,
+  name char(20) not null,
+  user_id char(20) not null,
+  time datetime not null
+  );
