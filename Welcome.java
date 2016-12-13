29ba2ab032b9bd9bd3c15648a831a660efdae2c5
jeonhyerin <marie0902@naver.com>
2016-12-13 19:46:26 +0900

    로그인 성공 화면
    로그인이 성공했을때 Welcome이라는 화면이 뜨도록 구현한 어플 액티비티입니다.

---
+++ Welcome.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Welcome extends Activity {
+    Button btnLogout;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_welcome);
+        btnLogout = (Button) findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                Intent login = new Intent(getApplicationContext(), MainActivity.class);
+                login.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(login);
+                finish();
+            }
+        });
+
+
+    }
+}
